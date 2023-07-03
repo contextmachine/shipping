1ec88771636957ea93f61d9389aae042d14e77e7
sth-v <aa@contextmachine.online>
2023-07-03 03:12:56 +0300

    fix ci/cd

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # This script should be used for local test builds, or for builds intended for local use.
 
 # Second Step (build image):
-DOCKER_BUILDKIT=1 docker build --rm --platform linux/amd64 -t shipping-test .
+DOCKER_BUILDKIT=1 docker build  --platform linux/amd64 -t shipping-test .
 
 
 # If you want to build arm64v8 comment this line,
@@ -9,4 +9,4 @@ DOCKER_BUILDKIT=1 docker build --rm --platform linux/amd64 -t shipping-test .
 
 #docker buildx build -build-arg NEXT_PUBLIC_URL="$NEXT_PUBLIC_URL" --platform linux/arm64v8  --tag "$IMAGE_REGISTRY/shipping-test:arm64v8" --push .
 
-DOCKER_BUILDKIT=1 docker run -p 0.0.0.0:3000:3000 --env-file .env.local --rm shipping-test
+DOCKER_BUILDKIT=1 docker run -p 0.0.0.0:3000:3000 --env-file .env --rm shipping-test
