359b198e293c6fe703fdba0e471fa3278251596f
antipov <antippov@gmail.com>
2023-08-18 22:16:27 +0800

    scale for mobile changed?

--- components/_Head.tsx
+++ components/_Head.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head"
 
-export default function _Head({title, description}: {title: string, description?: string}) {
+export default function _Head({ title, description }: { title: string, description?: string }) {
     return <Head>
         <title>{title}</title>
         <meta name="description" content={description ?? title} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <meta name="viewport" content="width=1280, initial-scale=1" />
+
     </Head>
 }
