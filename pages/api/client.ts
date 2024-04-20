23522ab557682dd275e4a31f885a20caccb0ba4e
sth-v <aa@contextmachine.ru>
2024-04-20 05:40:18 +0300

    fix ip

--- pages/api/client.ts
+++ pages/api/client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient("http://51.250.47.166:8080/v1/graphql", {
+export const client = new GraphQLClient("http://hasura.default.svc.cluster.local:8080/v1/graphql", {
     headers: { "x-hasura-admin-secret": "mysecretkey" },
 });
